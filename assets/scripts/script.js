bb76094705b5e1a5b09f016544ddeba8002d5c06
arVentura1 <andresruizventura1@gmail.com>
2022-10-21 14:16:51 +0200

    añadidos las cositas de animar las barras del menu hamburguesa

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,12 +2,31 @@
 
 $(function(){
 
+    // baner header menu
+    /*
+        //esto es para la animacion de las barras del menu hamburguesa
+        //
+        let hamburgerButton = document.querySelector(".hamburgerButton");
+        let barra = document.querySelectorAll(".barra");
+        //
+        $(hamburgerButton).click(function(){
+            barra.forEach(barra => barra.classList.toggle("x"));
+            //
+            //slider menu
+            //
+        });
+    */
+
+
+
+
     let corporativoRojo = '##F5333F';
     let rgbaCol = 'rgba(' + parseInt(corporativoRojo.slice(-6,-4),16)
         + ',' + parseInt(corporativoRojo.slice(-4,-2),16)
         + ',' + parseInt(corporativoRojo.slice(-2),16)
         +',0.1)';
 
+    // article wrapper
     $(".arti").click(function() {
         if (!$(this).children(".ca-flecha").hasClass("flecha-activa")) {
             $(this).children(".ca-flecha").addClass("flecha-activa");
@@ -20,4 +39,6 @@ $(function(){
 
         $(this).next('.arti-texto').slideToggle("fast");
     });
+
+
 });
